537f6c6642421580a7f180629c255ecf6d166b45
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2022-06-30 19:50:57 -0300

    adding navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+function NavBar() {
+    return(
+        <nav className="lf-navbar">
+            <h2>LaissezFaire</h2>
+            <ul className="lf-navUl">
+                <li>
+                    <a href="/">Home</a>
+                    
+                </li>
+                <li>
+                    <a href="/">Categories</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
